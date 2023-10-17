b452fc49d0247d3de21e17941636dcf1000baf49
demiryaylali <131900166+demiryaylali@users.noreply.github.com>
2023-10-17 19:08:26 +0300

    Add files via upload

---
+++ fen2.py
@@ -0,0 +1,16 @@
+print("ay evreleri hesaplama programına hoşgeldiniz!!")
+ay = int(input("ayın kaçıncı evresini görmek istersiniz/sadece sayı yazın!!"))
+if ay == 1:
+    print("ayın ilk evresi YENİ AYDIR!")
+    print("YENİ AYDA ayı göremeyiz çünkü GÜNEŞ ışıkları ayın arka kısmına geliyor!!")
+if ay == 2:
+    print("ilk dördün ayın 2. evresidir!!")
+    print("ilk dördün evresinde aya baktığımızda D şeklini görürüz")
+if ay == 3:
+    print("bu evrenin adı DOLUNAY!!")
+    print("DOLUNAYDA ayın her yerini görürüz!!")
+if ay == 4:
+    print("bu ayın son evresi SON DÖRDÜN!!")
+    print("bu evrede ayı ters D olarak görürüz!!")
+print("By Demir Yaylalı")
+print("bu proje Creative Commons 1.0 Universal(cco 1.0 universal) ile korunmaktadır.")
